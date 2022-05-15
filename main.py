6942e5f5de1a7780baac81c03ded50e43ea28317
Harsh09-A <84954376+Harsh09-A@users.noreply.github.com>
2022-05-15 15:40:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+global N
+N = 5
+
+def matricsboard(board):
+
+def checkqueensafe(board,row,col):
+
+def checkqueens(board,col):
+
+def s0lveNQ():
+    board = [[0,0,0,0,0],
+             [0,0,0,0,0],
+             [0,0,0,0,0],
+             [0,0,0,0,0],
+             [0,0,0,0,0]]
+    
+
+s0lveNQ()
